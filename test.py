2678a0c4ec8a0b3b6811d35c8fdb7937727498ac
zhangyan_laptop <zhangyantsxy@163.com>
2023-08-19 23:05:40 +0800

    update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 print("hello world")
+polling_active = True
+while polling_active:
+    num_1 = int(input("请输入一个数字："))
+    if num_1 == 'q':
+        break
+    num_2 = int(input("请输入第二个数字："))
+    if num_2 == "q":
+        break
+    n_sum = num_1+num_2
+    print(n_sum)
+    repeat = input("还想继续输入吗?(yes/no)")
+    if repeat == "no":
+        break
