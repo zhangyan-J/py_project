7ab2d19beb96e1ce8ae6407252e1eba1e5f7fe65
zhangyan <zhangyantsxy@163.com>
2023-09-04 23:04:24 +0800

    2023.9.4 update check_wav_from_xml.py

--- check_wav/check_wav_from_xml.py
+++ check_wav/check_wav_from_xml.py
@@ -1,52 +1,72 @@
 import os
-import xml.etree.ElementTree as ET
+import glob
+import re
+
+# 匹配文件夹下的所有 XML 文件
+folder_path = 'D:\project\py_project\check_wav'
 
 
-# 定义函数来遍历目录并获取XML文件
-def get_xml_files(xml_directory):
-    xml_files = []
-    for file_name in os.listdir(xml_directory):
-        if file_name.endswith('.xml'):
-            xml_files.append(os.path.join(xml_directory, file_name))
-    print(xml_files)
-    return xml_files
+# 递归遍历目录下的所有 XML 文件
+def traverse_xml_files(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        # print(f'{root}')
+        # print(f'dirs:{dirs}')
+        # print(f'files:{files}')
+        # print(root,dirs,files)
+        for file in files:
+            # print(os.path.join(root, file))
+            if file.endswith('xml'):
+                xml_file_path = os.path.join(root, file)
+                print(f"处理文件：{xml_file_path}")
+
+                # 遍历所有文件中的所有行
+                with open(xml_file_path, 'r', encoding='utf-8') as fin:
+                    xml_content = fin.read()
+                    pattern = r'<param name="path">(.*?)</param>'
+                    # 使用正则表达式进行匹配
+                    matches = re.findall(pattern, xml_content)
+                    # 打印匹配结果
+                    for match in matches:
+                        print(match)
+                    return matches
 
 
-# 定义函数来获取XML文件中使用的WAV文件名列表：
-def get_used_wav_filenames(xml_file):
-    used_wav_filenames = []
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-    for elem in root.iter():
-        if elem.tag == 'wav':
-            wav_filename = elem.text
-            used_wav_filenames.append(wav_filename)
-    print(used_wav_filenames)
-    return used_wav_filenames
+# 调用递归函数
+lista = traverse_xml_files(folder_path)
+print(lista)
+
+# 匹配文件夹下的所有 WAV 文件
+wav_path = 'D:\\project\\py_project\\check_wav\\audio'
+# print(wav_files)
+# # 打印生成的 WAV 文件列表
+print("------音频库--WAV--文件列表: ----------")
 
 
-# 定义函数来读取指定路径下的WAV文件名列表：
-def read_wav_filenames(file_path):
-    with open(file_path, 'r',encoding='utf-8') as f:
-        wav_filenames = f.read().splitlines()
-    print(wav_filenames)
-    return wav_filenames
+def get_wav_list(wav_path):
+    wav_files = glob.glob(wav_path + '/*.wav')
+    wav_lists = []
+    # print(wav_files)
+    for wav_file in wav_files:
+        # print(wav_file)
+        wav = wav_file.split('\\')
+        # print(wav)
+        wav_list = wav[-1]
+        # print(wav_list)
+        wav_lists.append(wav_list)
+    # print(wav_lists)
+    return wav_lists  ##函数中利用return返回列表，并且注意缩进，否则只打印一个元素
 
+
+listb = get_wav_list(wav_path)
+# print(f'------{listb}------')
 
 # 定义函数来对比两个WAV文件名列表：
-def compare_wav_filenames(used_wav_filenames, specified_wav_filenames):
-    common_filenames = set(used_wav_filenames) & set(specified_wav_filenames)
-    return list(common_filenames)
+def compare_wav_filenames(lista, listb):
+    for i in lista:
+        if i in listb:
+            print(f'音频:{i} 在 音频库-WAV文件列表中存在')
+        else:
+            print(f'音频:{i} 在 音频库-WAV文件列表中不存在')
 
-# 调用上述函数来执行遍历和对比操作：
-xml_directory = 'D:\\project\\py_project\\check_wav\\x-public'
-specified_wav_file = 'D:\\project\\py_project\\check_wav\\audio\\wav_list.txt'
-file_path = 'D:\\project\\py_project\\check_wav\\audio'
-# specified_wav_file = '/path/to/specified_wav_file.txt'
+compare_wav_filenames(lista,listb)
 
-xml_files = get_xml_files(xml_directory)
-used_wav_filenames = get_used_wav_filenames(xml_files)
-print(used_wav_filenames)
-# specified_wav_filenames = read_wav_filenames(specified_wav_file)
-
-common_filenames = compare_wav_filenames(used_wav_filenames, specified_wav_filenames)
