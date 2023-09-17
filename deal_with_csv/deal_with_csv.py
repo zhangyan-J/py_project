df7c3631385963b1c6de6aa635d43d0331ec79c9
zhangyan <zhangyantsxy@163.com>
2023-09-17 23:07:57 +0800

    2023.9.17 precode check 初版需求

--- deal_with_csv/deal_with_csv.py
+++ deal_with_csv/deal_with_csv.py
@@ -1,3 +1,4 @@
+import csv
 import sys
 import pandas as pd
 
@@ -30,17 +31,51 @@ import pandas as pd
 #
 # parse_csv(csv_file)
 
-data = [
-    ['liming','12','taian'],
-    ['zhangsan','20','heze'],
-    ['lisi','18','shenzhen']
-]
-output_file = '././output.txt'
-def write_csv(data,oupput_file,sep=','):
-    with open(oupput_file,'w') as fp:
-        for line in data:
-            #使用sep将每一行的数据连接起来
-            #同时在最后加上\n来进行换行
-            fp.write(sep.join(line)+'\n')
+# data = [
+#     ['liming','12','taian'],
+#     ['zhangsan','20','heze'],
+#     ['lisi','18','shenzhen']
+# ]
+# output_file = 'D:\project\py_project\deal_with_csv/output.txt'
+# def write_csv(data,oupput_file,sep=','):
+#     with open(oupput_file,'w') as fp:
+#         for line in data:
+#             #使用sep将每一行的数据连接起来
+#             #同时在最后加上\n来进行换行
+#             fp.write(sep.join(line)+'\n')
+#
+#
+# write_csv(data,output_file)
+# df = pd.DataFrame(data)
+# df.to_csv(output_file,header=None,index=None)
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # 这里保险的就是直接先把绝对路径加入到搜索路径
+sys.path.insert(0, os.path.join(BASE_DIR))
+sys.path.insert(0,os.path.join(BASE_DIR))
+sys.path.insert(0,os.path.join(BASE_DIR,'deal_with_csv'))# 把data所在的绝对路径加入到了搜索路径，这样也可以直接访问dataset.csv文件了
+# 这句代码进行切换目录
+os.chdir(BASE_DIR) # 把目录切换到当前项目，这句话是关键
 
-write_csv(data,output_file)
+print(os.getcwd())# 这样就能看到目前Python搜索路径在哪里，如果报错找不到，多半是你这个路径下没有文件
+# l = []
+# with open('data.csv','rt',encoding='utf-8') as f:
+#     cr = csv.reader(f)
+#     for row in cr:
+#         print(row)
+#         l.append(row)
+# with open('./test1.csv','wt',encoding='utf-8') as fw:
+#     cw = csv.writer(fw)
+#     ##采用writerow()方法
+#     for item in l:
+#         cw.writerow(item)#将列表的每个元素写道csv文件的一行
+    #或采用writerows()方法
+    # cw.writerows(l)#将嵌套列表内容写入csv文件，每个外层元素为一行，每个内层元素为一个数据
+
+l = []
+with open('test1.csv','rt',encoding='utf-8') as f:
+    cr = csv.DictReader(f)
+    for row in cr:
+        # print(row)
+        l.append(row) #将test2.csv内容读入列表l，每行为其一个元素，元素为dict
+                     #key为标题（未指定时为读取的第一行），value为对应列的数据
---
+++ precode_check/precode_check.py
@@ -0,0 +1,31 @@
+# 一期：通过读取excel文件中的每条用例的适用项目，检查是否每条用例都添加了语音Xpublic，语音Xmax，语音Xpro的precode，
+# 二期：检查每个用例的precode是否precode添加正确，是否有缺少的precode
+
+# 实现方式:
+# 1. 遍历每行用例，先把precode一列的数据提取处理，转换成一个列表
+# 2. 获取每条用例的适用项目，将使用项目生成一个列表，判断该用例的适用项目在此列表中属于哪个元素
+# 3. if in xpublic 适用项目应该有语音Xpublic，if in x-public-max适用项目应该有语音Xmax，if inx-public-pro，应有语音Xpro
+
+# import os
+import pandas as pd
+
+# a = os.getcwd()
+# print(a)
+
+file_path = r'D:\project\py_project\\precode_check\用例列表.xlsx'
+def read_excel(file_path):
+    df = pd.read_excel(file_path,sheet_name='Sheet1')
+    # print(df.head())
+    #读取指定的列
+    column_name = '适用项目'
+    #逐个取出指定列中的值
+    # for value in df[column_name]:
+    #     print(value)
+
+    #把读取到的指定列的数据生成一个list
+    mylist = df[column_name].tolist()
+    print(mylist)
+    return mylist
+
+read_excel(file_path)
+
