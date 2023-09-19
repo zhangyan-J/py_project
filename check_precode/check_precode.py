324f031a041e3b6895e6c77304679dafed8be869
zhangyan7 <zhangyan7@lixiang.com>
2023-09-19 12:57:11 +0800

    2023-09-19 静态代码检查-precode检查 v0.1.2

--- check_precode/check_precode.py
+++ check_precode/check_precode.py
@@ -13,27 +13,33 @@ import pandas as pd
 # print(a)
 
 file_path = r'D:\Practice\github\py_project\check_precode\用例列表.xlsx'
-# def read_excel(file_path):
-df = pd.read_excel(file_path,sheet_name='项目用例')
-# print(df.head())
-#读取指定的列
-column_name1 = '*适用项目'
-cloumn_name2 = 'precode'
-#逐个取出指定列中的值
-# for value in df[column_name]:
-#     print(value)
+class check:
+    def __init__(self,project_list,precode_list):
+        self.projec
 
-#把读取到的指定列的数据生成一个list
-project_list = df[column_name1].tolist()
-precode_list = df[cloumn_name2].tolist()
-print(project_list)
-print(precode_list)
+def read_excel(file_path):
+    df = pd.read_excel(file_path,sheet_name='项目用例')
+    # print(df.head())
+    #读取指定的列
+    column_name1 = '*适用项目'
+    cloumn_name2 = 'precode'
+    #逐个取出指定列中的值
+    # for value in df[column_name]:
+    #     print(value)
+
+    #把读取到的指定列的数据生成一个list
+    project_list = df[column_name1].tolist()
+    precode_list = df[cloumn_name2].tolist()
+    print(project_list)
+    print(precode_list)
     # return project_list
     # return precode_list
-for sublist in precode_list:
-    for item in sublist:
-        # print( f"Found {sublist} in the precode_list list!" )
-        print(item)
+    for sublist in precode_list:
+        if 'X-Public' == 'X-Public':
+            print('-----------''X-Public' in sublist )
+    #     for item in sublist:
+            # print( f"Found {sublist} in the precode_list list!" )
+            # print(item)
 
 
 
@@ -42,3 +48,23 @@ for sublist in precode_list:
 
 # read_excel(file_path)
 
+import pandas as pd
+
+
+class ExcelProcessor:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def check_column_data(self, sheet_name, condition_column, condition_value, target_column, target_content):
+        df = pd.read_excel( self.file_path, sheet_name=sheet_name )
+
+        condition_data = df[condition_column].tolist()
+        target_data = df[target_column].tolist()
+
+        for i in range( len( condition_data ) ):
+            if condition_data[i] == condition_value and target_data[i] != target_content:
+                print(
+                    f"Condition met in {condition_column} column, but {target_column} column does not have {target_content}." )
+                return
+
+        print( f"All conditions are satisfied." )
