7ef3c795539abb557b96979ce15735fc697b2a0e
zhangyan <zhangyantsxy@163.com>
2023-12-18 23:12:32 +0800

    2023.12.18 GUI 0.2

--- GUI_tools/demo_BMI/BMI_calculate.py
+++ GUI_tools/demo_BMI/BMI_calculate.py
@@ -1,55 +1,144 @@
+# from tkinter import *
+# from tkinter import messagebox
+#
+# def get_height():
+#     # 获取身高数据(cm)
+#     height = float(ENTRY2.get())
+#     return height
+#
+# def get_weight():
+#     # 获取体重数据(kg)
+#     weight = float(ENTRY1.get())
+#     return weight
+#
+# def calculate_bmi():
+#     # 计算BMI系数
+#     try:
+#         height = get_height()
+#         weight = get_weight()
+#         height = height / 100.0
+#         bmi = weight / (height ** 2)
+#     except ZeroDivisionError:
+#         messagebox.showinfo("提示", "请输入有效的身高数据!!")
+#     except ValueError:
+#         messagebox.showinfo("提示", "请输入有效的数据!")
+#     else:
+#         messagebox.showinfo("你的BMI系数是: ", bmi)
+#
+# if __name__ == '__main__':
+#     # 实例化object，建立窗口TOP
+#     TOP = Tk()
+#     TOP.bind("<Return>", calculate_bmi)
+#     # 设定窗口的大小(长 * 宽)
+#     TOP.geometry("400x400")
+#     # 窗口背景颜色
+#     TOP.configure(background="#8c52ff")
+#     # 窗口标题
+#     TOP.title("BMI 计算器")
+#     TOP.resizable(width=False, height=False)
+#     LABLE = Label(TOP, bg="#8c52ff", fg="#ffffff", text="欢迎使用 BMI 计算器", font=("Helvetica", 15, "bold"), pady=10)
+#     LABLE.place(x=55, y=0)
+#     LABLE1 = Label(TOP, bg="#ffffff", text="输入体重(单位：kg):", bd=6,
+#                    font=("Helvetica", 10, "bold"), pady=5)
+#     LABLE1.place(x=55, y=60)
+#     ENTRY1 = Entry(TOP, bd=8, width=10, font="Roboto 11")
+#     ENTRY1.place(x=240, y=60)
+#     LABLE2 = Label(TOP, bg="#ffffff", text="输入身高(单位：cm):", bd=6,
+#                    font=("Helvetica", 10, "bold"), pady=5)
+#     LABLE2.place(x=55, y=121)
+#     ENTRY2 = Entry(TOP, bd=8, width=10, font="Roboto 11")
+#     ENTRY2.place(x=240, y=121)
+#     BUTTON = Button(bg="#000000", fg='#ffffff', bd=12, text="BMI", padx=33, pady=10, command=calculate_bmi,
+#                     font=("Helvetica", 20, "bold"))
+#     BUTTON.grid(row=5, column=0, sticky=W)
+#     BUTTON.place(x=115, y=250)
+#     TOP.mainloop()
+
+### python 使用tkinter&subprocess 制作adb工具
 from tkinter import *
-from tkinter import messagebox
-
-def get_height():
-    # 获取身高数据(cm)
-    height = float(ENTRY2.get())
-    return height
-
-def get_weight():
-    # 获取体重数据(kg)
-    weight = float(ENTRY1.get())
-    return weight
-
-def calculate_bmi():
-    # 计算BMI系数
-    try:
-        height = get_height()
-        weight = get_weight()
-        height = height / 100.0
-        bmi = weight / (height ** 2)
-    except ZeroDivisionError:
-        messagebox.showinfo("提示", "请输入有效的身高数据!!")
-    except ValueError:
-        messagebox.showinfo("提示", "请输入有效的数据!")
+import subprocess
+import tkinter.filedialog
+import re
+def getDevicesVersion():
+    command_getVersion = 'adb shell getprop ro.build.version.release'
+    versionString = subprocess.Popen(command_getVersion, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+    versionString.wait()
+    versionString=versionString.decode()
+    versionString = re.split('\n', versionString)[0].strip('\r')
+    s='%s\n' % versionString
+    txt.insert(END, s)
+def getDevices():
+    command_getVersion = 'adb devices'
+    versionString = subprocess.Popen(command_getVersion, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+    versionString = versionString.decode()
+    txt.insert(END, '%s\n' % versionString)
+def getApkPackageName():
+     filename=tkinter.filedialog.askopenfilename()
+     if filename != '':
+         command = 'aapt d bading '+filename+' | grep package'
+         returnStr = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+         returnStr=returnStr.decode('gbk')
+     else:
+         txt.insert(END, '未选择文件')
+def listPackages():
+    command_getVersion = 'adb shell pm list packages'
+    versionString = subprocess.Popen(command_getVersion, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+    versionString=versionString.decode()
+    txt.insert(END, '%s\n' % versionString)
+def listPackages3():
+    command_getVersion = 'adb shell pm list packages -3'
+    versionString = subprocess.Popen(command_getVersion, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+    versionString=versionString.decode()
+    txt.insert(END, '%s\n' % versionString)
+def listPackagesSys():
+    command_getVersion = 'adb shell pm list packages -s'
+    versionString = subprocess.Popen(command_getVersion, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+    versionString=versionString.decode()
+    txt.insert(END, '%s\n' % versionString)
+def localInstall():
+    filename=tkinter.filedialog.askopenfilename()
+    if filename != '':
+         command = 'adb install '+filename
+         versionString = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+         versionString=versionString.decode()
+         txt.insert(END, '%s\n' % versionString)
     else:
-        messagebox.showinfo("你的BMI系数是: ", bmi)
+         txt.insert(END, '未选择文件')
+def getResolution():
+    command_getVersion = 'adb shell  wm size'
+    versionString = subprocess.Popen(command_getVersion, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+    versionString=versionString.decode()
+    txt.insert(END, '%s\n' % versionString)
+def getPixelDensity():
+    command_getVersion = 'adb shell wm density'
+    versionString = subprocess.Popen(command_getVersion, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.read()
+    versionString=versionString.decode()
+    txt.insert(END, '%s\n' % versionString)
+win = Tk()
+win.geometry('620x360')
+win.title('adb命令工具箱')
 
-if __name__ == '__main__':
-    # 实例化object，建立窗口TOP
-    TOP = Tk()
-    TOP.bind("<Return>", calculate_bmi)
-    # 设定窗口的大小(长 * 宽)
-    TOP.geometry("400x400")
-    # 窗口背景颜色
-    TOP.configure(background="#8c52ff")
-    # 窗口标题
-    TOP.title("BMI 计算器")
-    TOP.resizable(width=False, height=False)
-    LABLE = Label(TOP, bg="#8c52ff", fg="#ffffff", text="欢迎使用 BMI 计算器", font=("Helvetica", 15, "bold"), pady=10)
-    LABLE.place(x=55, y=0)
-    LABLE1 = Label(TOP, bg="#ffffff", text="输入体重(单位：kg):", bd=6,
-                   font=("Helvetica", 10, "bold"), pady=5)
-    LABLE1.place(x=55, y=60)
-    ENTRY1 = Entry(TOP, bd=8, width=10, font="Roboto 11")
-    ENTRY1.place(x=240, y=60)
-    LABLE2 = Label(TOP, bg="#ffffff", text="输入身高(单位：cm):", bd=6,
-                   font=("Helvetica", 10, "bold"), pady=5)
-    LABLE2.place(x=55, y=121)
-    ENTRY2 = Entry(TOP, bd=8, width=10, font="Roboto 11")
-    ENTRY2.place(x=240, y=121)
-    BUTTON = Button(bg="#000000", fg='#ffffff', bd=12, text="BMI", padx=33, pady=10, command=calculate_bmi,
-                    font=("Helvetica", 20, "bold"))
-    BUTTON.grid(row=5, column=0, sticky=W)
-    BUTTON.place(x=115, y=250)
-    TOP.mainloop()
+# 在窗体垂直自上而下位置50%处起，布局相对窗体高度50%高的文本框
+txt = Text(win)
+txt.place(rely=0.5, relheight=0.5)
+
+btn1 = Button(win, text='安卓版本', command=getDevicesVersion)
+btn1.place(relx=0.1, rely=0.1, relwidth=0.2, relheight=0.1)
+btn2 = Button(win, text='获取设备', command=getDevices)
+btn2.place(relx=0.1, rely=0.2, relwidth=0.2, relheight=0.1)
+btn3=Button(win,text='选择apk安装',command=localInstall)
+btn3.place(relx=0.1, rely=0.3, relwidth=0.2, relheight=0.1)
+btn4 = Button(win, text='查看手机安装包名', command=listPackages)
+btn4.place(relx=0.3, rely=0.1, relwidth=0.2, relheight=0.1)
+btn5 = Button(win, text='查看手机第三方包名', command=listPackages3)
+btn5.place(relx=0.3, rely=0.2, relwidth=0.2, relheight=0.1)
+btn6 = Button(win, text='查看手机系统包名', command=listPackagesSys)
+btn6.place(relx=0.3, rely=0.3, relwidth=0.2, relheight=0.1)
+btn7 = Button(win, text='查看手机分辨率', command=getResolution)
+btn7.place(relx=0.5, rely=0.1, relwidth=0.2, relheight=0.1)
+btn8 = Button(win, text='查看手机像素密度', command=getPixelDensity)
+btn8.place(relx=0.5, rely=0.2, relwidth=0.2, relheight=0.1)
+btn9 = Button(win, text='查看手机', command=listPackages)
+btn9.place(relx=0.5, rely=0.3, relwidth=0.2, relheight=0.1)
+
+win.mainloop()
--- adb_tool/set_up_log_leval.py
+++ adb_tool/set_up_log_leval.py
@@ -1,351 +1,91 @@
 # # -*- coding: utf-8 -*-
-# import os
-# import threading
-# import windnd
-# import tkinter as tk
-# from tkinter import *  # 导入 Tkinter 库
-#
-#
-# class APKInstaller:
-#
-#     def install_apk(self, device_name, apk_name):
-#         # 安装apk
-#         os.system("adb -s " + device_name + " install -r " + os.path.join(apk_name))
-#
-#     def get_device_list(self):
-#         os.system("adb devices")
-#         res = os.popen("adb devices").readlines()
-#         device_list = [sub.split('\t')[0] for sub in res[1:-1]]
-#         return device_list
-#
-#
-# # 此类用来制作界面
-# class APKTk():
-#
-#     def dragged_files(self, files):
-#         msg = '\n'.join((item.decode('gbk') for item in files))
-#         print(msg)
-#         self.apk_name.set(msg)
-#
-#     def __init__(self, APKInstaller):
-#         self.root = Tk()
-#         self.root.geometry('540x170')
-#         self.root.title("Execute_adb_command")
-#         self.apk_name = StringVar()
-#         self.apkInstaller = APKInstaller
-#         self.select_device_list = []
-#         self.device_list = self.apkInstaller.get_device_list()
-#         self.cb_list = []
-#         windnd.hook_dropfiles(self.root, self.dragged_files)
-#
-#     def mul_check_box(self):
-#         for index, item in enumerate(self.device_list):
-#             self.select_device_list.append(tk.StringVar())
-#             cb = Checkbutton(self.root, text=item, variable=self.select_device_list[-1], onvalue=item, offvalue='')
-#             self.cb_list.append(cb)
-#             cb.grid(row=index, column=0, sticky='w')
-#             cb.select()
-#
-#     # 刷新设备列表
-#     def refresh_data(self):
-#         print("refresh")
-#         for item in self.cb_list:
-#             item.grid_remove()
-#         self.cb_list = []
-#         self.device_list = self.apkInstaller.get_device_list()
-#         self.select_device_list = []
-#         self.mul_check_box()
-#         print("\n")
-#
-#     # 全选设备列表
-#     def select_all(self):
-#         for index, item in enumerate(self.device_list):
-#             self.device_list[index].set(item)
-#
-#     # 安装按钮
-#     def install_button(self):
-#         button_install = Button(self.root, text="安装", command=self.install)
-#         button_install.grid(row=len(self.device_list) + 1, column=1)
-#
-#     # 刷新设备列表按钮
-#     def refresh_button(self):
-#         button_install = Button(self.root, text="刷新", command=self.refresh_data)
-#         button_install.grid(row=len(self.device_list) + 1, column=2)
-#
-#     def input_text(self):
-#         entry_log = Entry(self.root, width=60, textvariable=self.apk_name)
-#         entry_log.grid(row=len(self.device_list) + 1, column=0, sticky='w')
-#
-#     def get_apk_name(self):
-#         return self.apk_name.get()
-#
-#     def mainloop(self):
-#         self.root.mainloop()
-#
-#     def install(self):
-#         selected_device_list = [i.get() for i in self.select_device_list if i.get()]
-#         print(selected_device_list)
-#         apkName = self.get_apk_name()
-#         print(apkName)
-#
-#         for device in selected_device_list:
-#             threading.Thread(target=self.apkInstaller.install_apk, args=(device, apkName)).start()
-#             print("启动" + device)
-#
-#
-# if __name__ == '__main__':
-#     apkInstaller = APKInstaller()
-#     apkTk = APKTk(apkInstaller)
-#     apkTk.mul_check_box()
-#     apkTk.input_text()
-#     apkTk.install_button()
-#     apkTk.refresh_button()
-#     apkTk.root.mainloop()
-
-
-
-
-# -*- coding: utf-8 -*-
-
-"""
-
-__author__:  @xxx
-
-__datetime__:  xxx
-
-"""
-
-import json
-
 import os
-
-from time import sleep
-
-# 生成导出任务配置
-
-import click
-
-import pandas as pd
-
-import requests
-import self as self
-from resume.resume import content
-
-# 导出表格地址
-
-EXPORT_OFFICE_URL= "https://doc.weixin.qq.com/v1/export/export_office"
-
-# 查询导出任务配置
-
-QUERY_PROGRESS_URL= "https://doc.weixin.qq.com/v1/export/query_progress"
-
-# 腾讯文档地址
-
-project_excel_url= "https://doc.weixin.qq.com/sheet/xxx"
-
-project_excel_name= "xxx表"
-
-class ProjectRemindRobot:
-
-    """导出腾讯文档到本地"""
-
-    def __init__(self,
-
-uid: str,
-
-uid_key: str,
-
-doc_id: str,
-
-file_name: str,
-
-wedrive_ticket: str,
-
-wedrive_skey: str,
-
-wedrive_sid: str
-
-                ):
-
-        self.uid= uid
-
-        self.uid_key= uid_key
-
-        self.doc_id= doc_id
-
-        self.file_name= file_name
-
-        self.wedrive_ticket= wedrive_ticket
-
-        self.wedrive_skey= wedrive_skey
-
-        self.wedrive_sid= wedrive_sid
-
-        # uid doc_id wedrive_skey wedrive_sid通常不变
-
-    def create_export_office_task(self):
-
-        """
-
-        创建导出文件任务"""
-
-        # 组织请求cookie并赋为全局变量方便后面接口使用
-
-        global cookie_value
-
-        cookie_value= 'uid=%s; uid_key=%s; wedrive_ticket=%s; wedrive_skey=%s; wedrive_sid=%s;' % (
-
-        self.uid,self.uid_key,self.wedrive_ticket,self.wedrive_skey,self.wedrive_sid)
-
-        doc_id_value= '%s' % self.doc_id
-
-        headers= {'content-type': 'application/x-www-form-urlencoded','Cookie': cookie_value}
-
-# 请求体
-
-        body= {'docId': doc_id_value,'version': '2'}
-
-#调用request发送post请求
-
-        response_body= requests.post(url=EXPORT_OFFICE_URL,headers=headers,data=body)
-        return response_body
-
-        print("添加导出任务返回内容为：", response_body.text)
-
-    def main(self):
-        pass
-
-
-def query_progress_task(self,operation_id):
-
-        """
-
-        查询导出文件任务进度"""
-
-        # 组织请求cookie
-
-        headers= {'content-type': 'application/x-www-form-urlencoded','Cookie': cookie_value}
-
-# 请求体
-
-        body= {'operationId': operation_id}
-
-#调用request发送get请求
-
-        response_body= requests.get(url=QUERY_PROGRESS_URL,headers=headers,params=body)
-
-        print("查询进度内容为：", response_body.text)
-
-        return response_body
-
-def down_file(self,file_url):
-
-        """
-
-        下载文件"""
-
-        headers= {'Cookie': cookie_value}
-
-        response_body= requests.get(url=file_url,headers=headers)
-        
-        print("查询进度内容为：", response_body)
-        
-        return response_body
-
-def read_data(self):
-
-        io= r'C:\Users\user\PycharmProjects\xxx\test.xlsx'
-
-        # 读取表格，只读取sheet_name为xx，以第二行为表头，只读取指定行
-
-        data= pd.read_excel(io,sheet_name='xxx',header=1,usecols=[0,1,2,5,6,7,8])
-
-        return data
-
-def main(self):
-
-        """
-
-        程序入口"""
-
-        # 1、创建任务
-
-        resp_create_export_office= self.create_export_office_task()
-
-        print("添加导出任务返回内容为：%s" % resp_create_export_office.text)
-        
-        operation_id= resp_create_export_office.json()["operationId"]
-        
-        print("创建任务成功：%s" % operation_id)
-
-# 2、轮询任务，直到 100% 停止
-
-        while True:
-
-            resp_query_progress= self.query_progress_task(operation_id)
-
-            progress= resp_query_progress.json()["progress"]
-            
-            print("|" + "#" * progress+ "| " + "%s/100" % progress)
-            
-            if progress>= 100:
-            
-                            file_url= resp_query_progress.json()["file_url"]
-            
-            print("导出任务完成：%s" % resp_query_progress)
-            
-            break
-            
-            sleep(1)
-
-# 3、下载文件
-
-        print("开始下载文件。。。")
-
-        content= self.down_file(file_url)
-        
-        print(type(content))
-
-        file_name= "test.xlsx"
-        
-                # 若存在则先删除文件
-        
-        if os.path.exists(file_name):
-    
-            os.remove(file_name)
-
-# 写入文件
-
-        with open(file_name,"wb")as code:
-
-            code.write(content.content)
-
-            print("下载完成。")
-            
-            # 读取文件
-            
-            self.read_data()
-
-if __name__== '__main__':
-
-    # 只需要定时替换uid_key wedrive_ticket
-
-    prr= ProjectRemindRobot(
-
-    uid="xxx",
-    
-    doc_id="xx",
-    
-    file_name="export_file.xlsx",
-    
-    wedrive_skey="xx",
-    
-    wedrive_sid="xx",
-    
-    uid_key="xxx",
-    
-    wedrive_ticket="xxx",
-    
-    )
-    
-    prr.main()
+import re
+import logging
+from logging import handlers
+import time
+def check_device_type():
+    # 获取当前所有连接的adb设备
+    def check_current_adb():
+        device_list = []
+        device_re = r'(.*)	device'
+        command = 'adb devices'
+        r = os.popen(command)
+        info = r.readlines()
+        for line in info:
+            line = line.strip('\r\n')
+            search_result = re.match(device_re, line, re.M | re.I)
+            if search_result:
+                device_list.append(search_result.group(1))
+        return device_list
+
+    # 设备信息保存字典中
+    device_type_dict = {'x_front_8155': '', 'x_rear_8155': '', 'phone': '', 'm01': ''}
+    for i in range(0, len(check_current_adb())):
+        # 如果设备名称以f或者r结尾，则为x01的前或后8155设备
+        if 'f' == check_current_adb()[i][-1]:
+            device_type_dict['x_front_8155'] = check_current_adb()[i]
+        elif 'r' == check_current_adb()[i][-1]:
+            device_type_dict['x_rear_8155'] = check_current_adb()[i]
+        # 除此之外的设备，名称长度大于8的认为是手机设备，长度等于8的认为m01设备
+        else:
+            if len(check_current_adb()[i]) > 8:
+                device_type_dict['phone'] = check_current_adb()[i]
+            if len(check_current_adb()[i]) == 8:
+                device_type_dict['m01'] = check_current_adb()[i]
+    log.logger.info('flash_all_build: check_device_type: all device id is ' + str(device_type_dict))
+    return device_type_dict
+def runcmd(adb_command):
+    os.system('adb -s ' + device_id + ' root')
+    time.sleep(1)
+    os.system('adb -s ' + device_id + ' shell')
+    if device_id[-1] == "r":
+        log.logger.info('flash_all_build: runcmd: ' + device_id +
+                        ' shell ')
+        os.system('adb -s ' + device_id + ' shell QFirehose -s /sys/bus/usb/devices/1-1 -f /data/modemfw')
+    else:
+        log.logger.info('flash_all_build: flash_5g: ' + device_id +
+                        ' shell QFirehose -f /data/modemfw')
+        os.system('adb -s ' + device_id + ' shell QFirehose -f /data/modemfw')
+
+
+if __name__ == '__main__':
+    try:
+        device_info_dict = check_device_type()
+        download_build_name_lst, flash_item_name_lst, car_type = download_need_build()
+        if car_type == "X01" or car_type == "X02-Max" or car_type == "W01":
+            if 'mcu' in flash_item_name_lst and device_info_dict['x_front_8155'] != '':
+                log.logger.info('flash_all_build: main: start flash mcu')
+                flash_udisk(device_info_dict['x_front_8155'], download_build_name_lst, "MCU")
+            if '5G' in flash_item_name_lst and device_info_dict['x_rear_8155'] != '':
+                log.logger.info('flash_all_build: main: start flash rear_8155')
+                flash_5g(device_info_dict['x_rear_8155'], download_build_name_lst)
+            if 'rear_8155' in flash_item_name_lst and device_info_dict['x_rear_8155'] != '':
+                log.logger.info('flash_all_build: main: start flash rear_8155')
+                flash_8155(device_info_dict['x_rear_8155'], download_build_name_lst, car_type)
+            if 'front_8155' in flash_item_name_lst and device_info_dict['x_front_8155'] != '':
+                log.logger.info('flash_all_build: main: start flash front_8155')
+                flash_8155(device_info_dict['x_front_8155'], download_build_name_lst, car_type)
+        elif car_type == "X02-Pro":
+            if 'front_8155' in flash_item_name_lst and device_info_dict['x_front_8155'] != '':
+                log.logger.info('flash_all_build: main: start flash front_8155')
+                flash_8155(device_info_dict['x_front_8155'], download_build_name_lst, car_type)
+            if '5G' in flash_item_name_lst and device_info_dict['x_front_8155'] != '':
+                # log.logger.info('flash_all_build: main: start flash x_front_8155')
+                # if os.path.exists(os.path.join(os.getcwd(), "no_code_change_and_last_version_path.txt")):
+                #     flash_5g(device_info_dict['x_front_8155'], download_build_name_lst)
+                #     time.sleep(180)
+                # else:
+                #     flash_udisk(device_info_dict['x_front_8155'], download_build_name_lst, "5G")
+                #     time.sleep(600)
+                # log.logger.info("ota 5G finished, waiting for starting up, then flashing mcu")
+                log.logger.info('flash_all_build: main: start flash x_front_8155')
+                flash_5g(device_info_dict['x_front_8155'], download_build_name_lst)
+                log.logger.info("ota 5G finished, waiting for starting up, then flashing mcu")
+                time.sleep(180)
+            if 'mcu' in flash_item_name_lst and device_info_dict['x_front_8155'] != '':
+                log.logger.info('flash_all_build: main: start flash mcu')
+                flash_udisk(device_info_dict['x_front_8155'], download_build_name_lst, "MCU")
+        os.system("taskkill /F /IM cmd.exe")
+    except Exception as e:
+        log.logger.error('flash_all_build: main: failed reason is %s' % e.args)
