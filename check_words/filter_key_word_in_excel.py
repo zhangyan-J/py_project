c1a9a0b54e0de63f57b6545c81b0980a3e157fa8
zhangyan <zhangyantsxy@163.com>
2024-04-11 23:20:35 +0800

    2024.4.11 更新 检查txt/excel中关键字出现次数

--- check_words/filter_key_word_in_excel.py
+++ check_words/filter_key_word_in_excel.py
@@ -1,22 +1,25 @@
-import openpyxl
 import pandas as pd
-df = pd.read_excel(r'D:\zhangyan7\Downloads\P0P1P1常态化自动化用例.xlsx',sheet_name='脚本用例模版')
-# keyword = input("请输入要搜索的关键字：")
-keyword = '清洁模式'
-# #遍历每一列
-# for column in df.columns:
-#     #遍历该列的每一个元素
-#     for value in df[column]:
-#         if keyword in str(value):
-#             # print(f"找到关键字{keyword}在列{column}中'f'{value}'中找到。")
+
+# 读取Excel文件
+file_path = r"D:\project\py_project\豆瓣电影Top250.xls"  # 替换为你的Excel文件路径
+if file_path.endswith('.xls'):
+    engine = 'xlrd'
+
+elif file_path.endswith('.xlsx'):
+    engine = 'openpyxl'
+
+else:
+    raise ValueError("Unsupported file format")
+
+df = pd.read_excel(file_path, engine=engine)
+
+# 输入关键字
+keyword = input("请输入关键字：")
+# 遍历DataFrame中的每个单元格
 for index, row in df.iterrows():
-    for column_value in row.values:
+    for col_index, column_value in row.items():
         if keyword in str(column_value):
-            print(f"找到关键字：{keyword} 在第 {index+1} 行，第 {column_value} 列中")
+            print(f"找到关键字 '{keyword}' 在第 {index + 1} 行 {col_index} 列")
 
-
-
-# if __name__ == '__main__':
-#     # 示例用法
-#     file_path = r'D:\zhangyan7\Downloads\P0P1P1常态化自动化用例.xlsx'  # 替换为你的Excel文件路径
-#     keyword = '清洁模式'
+# 查找关键字
+# find_keyword_in_cell(df, keyword)
