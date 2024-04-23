b569f2136022a861d83ba95293c43442706c06b3
zhangyan7 <zhangyan7@lixiang.com>
2024-04-24 00:31:59 +0800

    2024-4-24 查找脚本引用的fun

--- filter_xml_file/match_func_and_xml.py
+++ filter_xml_file/match_func_and_xml.py
@@ -28,6 +28,7 @@ def traverse_xml_files(folder_path):
 
 xml_files_list = traverse_xml_files(folder_path)
 
+
 def map_xml(xml_files_list):
     fun_list = []
     # 遍历 XML 文件列表
@@ -44,43 +45,49 @@ def map_xml(xml_files_list):
                 fun_list.append(match)
     with open(r'D:\practice\github\py_project\filter_xml_file\fun_list.txt', 'w', encoding='utf-8') as file:
         [file.write(str(match) + '\n') for match in fun_list]  ##将list(列表)保存为txt文件自动换行
-        ##去重
-        old_list = fun_list
-        new_list = list(dict.fromkeys(old_list))
-        [file.write(str(match) + '\n') for match in new_list]  ##将list(列表)保存为txt文件自动换行
-
         file.close()
-    return fun_list
+    # 去除重复元素
+
+    new_fun_list = list(set(fun_list))
+    with open(r'D:\practice\github\py_project\filter_xml_file\new_fun_list.txt', 'w', encoding='utf-8') as file:
+        [file.write(str(fun) + '\n') for fun in new_fun_list]  ##将list(列表)保存为txt文件自动换行
+        # file.write('\n'.join(new_fun_list))
+        # print(new_fun_list)
+        file.close()
+    # print(type(fun_list))
+    # new_fun_list = list({}.fromkeys(fun_list).keys())
+    return new_fun_list
 
 
 map_xml(xml_files_list)
-# print(xml_files_list)
-# file_path = './output.txt'
-# # 打开文件以写入模式，并逐行写入列表元素
-# with open(file_path, 'w') as file:
-#     for item in xml_files_list:
-#         file.write(item + '\n')
-# 在循环之外遍历 XML 文件列表
-# assert_list = []
-# for xml_file_path in xml_files_list:
-#     # print("处理文件：", xml_file_path)
-#     # 在这里对每个 XML 文件进行操作
-#     with open(xml_file_path, 'r', encoding='utf-8') as xml_file:
-#         xml_content = xml_file.read()
-#         # pattern = r'<param name="path">(.*?)</param>'
-#         pattern = r'<param name="expected">([^<>]*)</param>'
-#         # pattern = '<param name="audio_name">(.*?)</param>'  ##问题所在，匹配的正则表达式不正确，下一个问题是然后匹配多个正则表达[2023.09.07]
-#         # 使用正则表达式进行匹配
-#         matches = re.findall(pattern, xml_content)
-#         for match in matches:
-#             # print(match)
-#             assert_list.append(match)
-#
-# # print(assert_list)
-# print("------音频库--WAV--文件列表: ----------")
-#
-#
-#
+
+folder_path = r'D:\GIT\AutomationTest\Project\SS全平台\Public\Script_Resource\Functions'
+def check_dict_xml(folder_path):
+
+    file_names = []
+    for root, dirs, files in os.walk(folder_path):
+        files[:] = [f for f in files if f.endswith(".xml")]
+        # print(files)
+        for file in files:
+            xml_file_path = os.path.join(root, file)
+            # print( f"处理文件：{xml_file_path}" )
+            file_names.append(xml_file_path)
+            # print(xml_files)
+    fun_names = []
+    for fun_name in file_names:
+        fun_name_before = fun_name.split('\\')[-1]
+        last_fun_name = fun_name_before.split('.')[0]
+        fun_names.append(last_fun_name)
+        print(fun_names)
+    # 读取字典文件
+    with open(r'D:\practice\github\py_project\filter_xml_file\fun_names.txt', 'w', encoding='utf-8') as file:
+        [file.write(str(fun_name) + '\n') for fun_name in fun_names]
+        # file.write('\n'.join(fun_names))
+        file.close()
+    # return name
+
+
+check_dict_xml(folder_path)
 # old_list = assert_list
 # new_list = list(dict.fromkeys(old_list))
 # print(new_list) # [2, 3, 4, 5, 1]
