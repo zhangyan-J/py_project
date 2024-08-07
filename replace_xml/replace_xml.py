a6aadacec36d5998344684fabe748a4df0baafd1
zhangyan <zhangyantsxy@163.com>
2024-08-07 23:37:00 +0800

    2024.8.07 替换单个xml文件内属性名

--- replace_xml/replace_xml.py
+++ replace_xml/replace_xml.py
@@ -1,141 +1,119 @@
-# import lxml
-# import lxml.etree as ET
-import os
-#
-#
 # #
-# def read_batfile(bat_path):
-#     with open(bat_path, 'r') as file:
-#         return file.read()
 #
+#
+# import os
+# import xml.etree.ElementTree as ET
+#
+# # 指定目录路径
+# directory_path = r"D:\project\py_project\replace_xml\x-public"
+#
+#
+# def get_xml_file_name(directory):
+#     file_names = []
+#     # 遍历目录下的所有文件
+#     for filename in os.listdir(directory):
+#         if filename.endswith(".xml"):  # 只处理XML文件
+#             xml_file = os.path.join(directory, filename)
+#             # print(xml_file)
+#             file_name_without_extension = xml_file.split('\\')[-1].split('.')[0]
+#             # print(file_name_without_extension)
+#             file_names.append(file_name_without_extension)
+#     return file_names
+#
+#
+# # 调用函数
+# replacement_values = get_xml_file_name(directory_path)
+# print(replacement_values)
 # #
-# # # Example usage
-# # bat_file = read_batfile(r'D:\project\py_project\replace_xml\demo.bat')
-# # print(bat_file)
-# # xml = read_xml(r'D:\project\py_project\replace_xml\input.xml')
-# # print(xml)
-# # # output = open(r'D:\project\py_project\replace_xml\output.xml', 'w')
-# # # output.write(xml)
-# # # # Output: <root><batfile>echo Hello World</batfile></root>
-#
-#
-# import xml.etree.ElementTree as et
-#
-#
-# # print(et.ElementTree(file=r'D:\project\py_project\replace_xml\input.xml'))
-#
-# # xml_path = et.ElementTree(file=r'D:\project\py_project\replace_xml\input.xml')
-#
-def traverse_xml_files(folder_path):
-    # global xml_files
-    xml_files = []
-    for root, dirs, files in os.walk(folder_path):
-        files[:] = [f for f in files if f.endswith(".xml")]
-        # print(files)
-        for file in files:
-            xml_file_path = os.path.join(root, file)
-            # print( f"处理文件：{xml_file_path}" )
-            xml_files.append(xml_file_path)
-            # print(xml_files)
-            # print(len(xml_files))
-            new_file_list = []
-            new_files = xml_files[-1].split('\\')[-1].split('.')[0]
-            new_file_list.append(new_files)
-            # print(type(new_file_list))
-            # print( f"处理文件：{new_file_list}")
-            return new_file_list
-            # for file_name in new_files:
-                # print(f"处理文件：{file_name}")
-                # return file_name
-                # print(f"处理文件：{file_name}")
-folder_path = r'D:\project\py_project\filter_xml_file\x-public'
+# # # print(pxf)
 # #
-xml_files_list = traverse_xml_files(folder_path)
-# # print(f"文件数量：{len(xml_files_list)}")
-print(xml_files_list)
-#
-#
-# def get_root_name(file_path):
-#     # file_path =r'D:\project\py_project\replace_xml\input.xml'
-#     # tree = et.ElementTree(file=file_path)
-#     tree = et.parse(file_path)
-#     root = tree.getroot()
-#
-#     def traverse_element(element,depth=0):
-#         ident = ' ' * depth
-#         # print(f"{ident}Tag: {element.tag},Attribute: {element.attrib}")
-#         for child in element:
-#             traverse_element(child, depth + 1)
-#
-#     traverse_element(root)
-#
-#     for root_tag in root:
-#         root_tag = root_tag.attrib['name']
-#         # return root_tag
-#         # root_tag = root.find('name')
-#         return root_tag
-#
-# def main():
-#     folder_path = r'D:\project\py_project\filter_xml_file\x-public'
-#     file_extension = '.xml'
-#
-#     for file_name in os.listdir(folder_path):
-#         if file_name.endswith(file_extension):
-#             file_path = os.path.join(folder_path, file_name)
-#             title = get_root_name(file_path)
-#             # get_root_name(file_path)
-#
-#             print(f"Title in xml_file of {file_name} is : {title}")
-#
-#             # file_name = f'{title}'
-#             # print(f"Added filename to {file_name}")
-#             # print(new_root_name)
-#             # new_file_name = f'{new_root_name}.xml'
-#
-#
-# if __name__ == '__main__':
-#     main()
+# #
+# # def parase_xml_file():
+# #     attribute_names = []
+# #     for file_name in replacement_values:
+# #         xml_file = os.path.join(directory_path, file_name + ".xml")
+# #         # print(xml_file)
+# #         # 解析XML文件
+# #         tree = ET.parse(xml_file)
+# #         root = tree.getroot()
+# #         for tag in root:
+# #             attribute_names.append(tag.attrib['name'])
+# #     return attribute_names
+# #
+# #
+# # attribute_name = parase_xml_file()
+# #
+# #
+# # # print(attribute_names)
+# #
+# # def replace_attribute_in_xml(file_path, values):
+# #     # 解析 XML 文件
+# #     tree = ET.parse(file_path)
+# #     root = tree.getroot()
+# #     el = root.iter()
+# #     # print(root.attrib)
+# #     # 获取所有要替换的标签
+# #     elements = list(root.iter())
+# #     # print(elements)
+# #     # print(elements)
+# #     # 确保有足够的值进行替换
+# #     num_elements = len(elements)
+# #     num_values = len(values)
+# #     # # print(num_elements,num_values)
+# #     # if num_values < num_elements:
+# #     #     raise ValueError("Replacement values list is too short for the number of XML elements")
+# #
+# #     # 替换指定属性的值
+# #     for i, elem in enumerate(elements):
+# #         if replacement_values not in elem.attrib:
+# #             # print(attribute_name)
+# #             elem.attrib[attribute_name] = values[i % replacement_values]
+# #
+# #     # 保存修改后的 XML 文件
+# #     tree.write(file_path, encoding='utf-8', xml_declaration=True)
+# #
+# #
+# # # 批量处理目录中的所有 XML 文件
+# # for filename in os.listdir(directory_path):
+# #     if filename.endswith('.xml'):
+# #         file_path = os.path.join(directory_path, filename)
+# #         replace_attribute_in_xml(file_path, replacement_values)
+# #
+# # replace_attribute_in_xml()
+# # print("所有 XML 文件已处理。")
+
 
 import os
 import xml.etree.ElementTree as ET
 
 
-def get_fun_name(file_path):
-    tree = ET.parse(file_path)
+def check_xml_filename(xml_file):
+    # 获取文件名
+    file_name = os.path.splitext(os.path.basename(xml_file))[0]
+    return file_name
+    # 检查文件名和属性名是否一致
+    # if file_name != root[0].attrib["name"]:  # 如果不一致，将属性名改为文件名
+    #     root[0].attrib["name"] = file_name
+    # print(f"属性名已更改为：{file_name}")
+
+def main():
+    # 读取XML文件
+    xml_file = r"D:\project\py_project\replace_xml\x-public\netease_delete_music.xml"
+    tree = ET.parse(xml_file)
     root = tree.getroot()
+    # # 获取function的name属性：即function名称
+    # # fun_name = root[0].attrib["name"]
+    # # print(fun_name)
+    file_name = check_xml_filename(xml_file)
+    if root[0].attrib["name"] != file_name:
+        root[0].attrib["name"] = file_name
+    # 遍历XML元素，检查文件名和属性名是否一致
+    for elem in root.iter():
+        check_xml_filename(xml_file)
 
-    for tag in root:
-        # print(tag.tag, tag.attrib, tag.text)
-        # tag.attrib['function name'] = tag.text
-        # return tag
-        if tag.text is not None:
-            return tag
-
-file_path = r'D:\project\py_project\filter_xml_file\x-public\X_common_voice_wake_up.xml'
-
-
-# def get_file_name():
-#     for path in xml_files_list:
-#         # if path is not None:
-#         path = xml_files_list[0]
-#         # print(path)
-#         file_name = path.split('\\')[-1].split('.')[0]
-#         return file_name
-    # get_fun_name(path)
+    # 保存更改后的XML文件
+    tree.write(xml_file)
 
 
-
-if __name__ == '__main__':
-    # path = get_fun_name(file_path)
-    # file_name = traverse_xml_files(folder_path)
-    # print(file_name)
-    get_fun_name(file_path)
-    elem = get_fun_name(file_path).tag, get_fun_name(file_path).attrib,get_fun_name(file_path).text
-    for key in elem[1].values():
-        # print(key)
-        # for xml_file_path in xml_files_list:
-        #     get_fun_name(xml_file_path)
-        if key is not None and key != file_name:
-            key.replace(key,file_name)
-    # print(get_fun_name(file_path))
-
+if __name__ == "__main__":
+    main()
